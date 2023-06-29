0f075e36613849d398382a1fc2cd3fce9fc87947
nattawut.sin <nattawut42949@hotmail.co.th>
2023-06-29 16:18:48 +0700

    test

--- cheese_profile/src/mainpage.jsx
+++ cheese_profile/src/mainpage.jsx
@@ -31,10 +31,84 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
 
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import P1 from './img/set1/1.jpg'
+
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
+
+function srcset(image, size, rows = 1, cols = 1) {
+  return {
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${size * cols}&h=${
+      size * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+  };
+}
+
+const itemData = [
+  {
+    img: 'https://cdn.pic.in.th/file/picinth/1c2799a68089f023e.jpeg',
+    title: 'Breakfast',
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: {P1},
+    title: 'Burger',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    title: 'Camera',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    title: 'Coffee',
+    cols: 2,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+    title: 'Hats',
+    cols: 2,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    title: 'Honey',
+    author: '@arwinneil',
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+    title: 'Basketball',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+    title: 'Fern',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+    title: 'Mushrooms',
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+    title: 'Tomato basil',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+    title: 'Sea star',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+    title: 'Bike',
+    cols: 2,
+  },
+];
 
 function MainPage() {
   const [value, setValue] = useState(0);
@@ -76,6 +150,7 @@ function MainPage() {
           />
           <Typography variant="h1" component="h2">
           </Typography>
+        
         </div>
         {/* <List>
           {messages.map(({ primary, secondary, person }, index) => (
@@ -141,9 +216,24 @@ function MainPage() {
         onClose={handleClose2}
         aria-describedby="alert-dialog-slide-description"
       >
-        <DialogTitle>{"ทำความรู้จักพี่ชีส 2"}</DialogTitle>
+        <DialogTitle>{"ทำความรู้จักพี่ชีส"}</DialogTitle>
         <DialogContent>
-        
+        <ImageList
+      sx={{ width: 500, height: 450 }}
+      variant="quilted"
+      cols={4}
+      rowHeight={121}
+    >
+      {itemData.map((item) => (
+        <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
+          <img
+            {...srcset(item.img, 121, item.rows, item.cols)}
+            alt={item.title}
+            loading="lazy"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose2}>OK</Button>
@@ -211,5 +301,6 @@ const messageExamples = [
       for my backyard and would love to fire up the grill.`,
     person: '/static/images/avatar/1.jpg',
   }];
+
 
 export default MainPage
